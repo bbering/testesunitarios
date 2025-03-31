60a2a0806e829c38cb18d3e14b7d18d2a74569d9
bbering <bbering88@gmail.com>
2025-03-31 19:10:50 -0300

    criacao da model e adicionad adependencia do spring boot starter test

---
+++ src/main/java/com/application/testesunitarios/model/Book.java
@@ -0,0 +1,29 @@
+package com.application.testesunitarios.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "releaseYear")
+    private int releaseYear;
+
+    @Column(name = "quantityAvailable")
+    private int quantityAvailable;
+}
