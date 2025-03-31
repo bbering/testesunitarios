d92454bf8156ff0cc39f803adf9d5eafda00947a
bbering <bbering88@gmail.com>
2025-03-31 18:52:52 -0300

    push dos arquivos gerados pelo initializr

---
+++ src/test/java/com/application/testesunitarios/TestesunitariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.testesunitarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestesunitariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
